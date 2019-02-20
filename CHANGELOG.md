5842ca94c531d790b472818a5b69c69947e2e025
Kyriakos Stavrakidis <kyriakos.stavrakidis@cosmosdirekt.de>
2019-02-20 16:12:38 +0100

    Some internal changes

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog history
+
+## pd_multiprocessing 1.0.2 (2019-02-20)
+### Trivial/Internal Changes
+- Changes due to pypi
+- Rename of some text files
+- Changelog is now written in markdown
+
+## pd_multiprocessing 1.0.2 (2019-02-20)
+### Trivial/Internal Changes
+- Changes due to pypi
+
+
+## pd_multiprocessing 1.0.1 (2019-02-20)
+### Features
+- Compatibility with python 3.4
+
+### Trivial/Internal Changes
+- Modification for using travis ci and coveralls.io
+
+
+## pd_multiprocessing 1.0.0 (2019-02-19)
+- Initial release
--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ setup.py of this project
 from setuptools import setup
 
 
+CLASSIFIERS = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+]
+
 setup(
-    name='pd_multiprocessing',
-    version='1.0.2',
-    author='Kyriakos Stavrakidis',
-    author_email='kstavrakidis@gmail.com',
-    description='Simple function to parallelize the mapping function of Pandas DataFrame',
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url='https://github.com/stavrakidis/pd_multiprocessing',
-    license='LICENSE.txt',
-    packages=['pd_multiprocessing', 'pd_multiprocessing.test'],
+
     install_requires=[
         'pandas>=0.22.0',
         'pytest>=3.4.1'
