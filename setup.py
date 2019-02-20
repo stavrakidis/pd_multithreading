49b1105eb90decd02317a080b10404f0eab18555
Kyriakos Stavrakidis <kyriakos.stavrakidis@cosmosdirekt.de>
2019-02-20 15:09:41 +0100

    Changes due to pypi

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools import setup
 
 setup(
     name='pd_multiprocessing',
-    version='1.0.1',
+    version='1.0.2',
     author='Kyriakos Stavrakidis',
     author_email='kstavrakidis@gmail.com',
     description='Simple function to parallelize the mapping function of Pandas DataFrame',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/stavrakidis/pd_multiprocessing',
     license='LICENSE.txt',
     packages=['pd_multiprocessing', 'pd_multiprocessing.test'],
